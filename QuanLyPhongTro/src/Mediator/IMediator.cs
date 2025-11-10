fcdd6b0632b0d84d8b5d5fc6e4ad9b49f15db7ec
Khang <giakhang111103@gmail.com>
2025-11-10 17:23:18 +0700

    Finish Mediator v1

--- QuanLyPhongTro/src/Mediator/IMediator.cs
+++ QuanLyPhongTro/src/Mediator/IMediator.cs
@@ -6,11 +6,14 @@ using System.Threading.Tasks;
 
 namespace QuanLyPhongTro.src.Mediator
 {
-    internal interface IMediator
+    public interface IMediator
     {
         public void Register<TMessage>(string subriberKey, Func<TMessage, Task> handler);
         public void Unregister(string subriberKey);
         public Task Publish<TMessage>(string key, TMessage message);
         public void RegisterFactory(string key, Func<Control> factory);
+        public Task PublishForm<TMessage>(string key, TMessage message, Action<Control> ControlReady);
+        public Task PublishList<TMessage>(string key, IEnumerable<TMessage> message, Action<List<Control>> ControlReady);
+        public void Dispose<TMessage>();
     }
 }
